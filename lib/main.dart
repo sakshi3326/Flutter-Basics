5692502169eac586c1ead150add122984e0fd08d
sakshi3326 <saqkshiiitian123@gmail.com>
2022-08-10 02:59:37 +0530

    padding and margin with widget container

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,14 @@ class home extends StatelessWidget {
         centerTitle: true,
         backgroundColor: Colors.red[600],
       ),
-      body: Center(
-        child: IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.alternate_email),
-          color: Colors.amber,
-        )
+      body: Container(
+        //padding: EdgeInsets.all(20.0),
+        //padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+       padding: EdgeInsets.fromLTRB(10.0, 20.0, 10.0, 20.0),
+        margin: EdgeInsets.all(60.0),
+
+        color: Colors.grey[400],
+        child: Text('hello'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
